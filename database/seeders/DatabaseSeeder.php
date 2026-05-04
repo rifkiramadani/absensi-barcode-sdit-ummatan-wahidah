18f32b9b5e81fc7ee302463feb40cfb267bc65c5
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2026-05-04 14:04:31 +0700

    feat: configure user and setting seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Database\Seeders\SettingSeeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +16,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $this->call(SettingSeeder::class);
+
+        User::factory(1)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin sdituw',
+            'email' => 'admin@example.com',
         ]);
     }
 }
