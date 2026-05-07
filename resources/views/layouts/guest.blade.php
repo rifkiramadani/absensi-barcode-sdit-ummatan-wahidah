cbba2fba8de1ee2775186e0a10a1dc1bbd43a302
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2026-05-07 15:40:40 +0700

    changing title of the website

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Sistem Absensi SDIT Ummatan Wahidah') }}</title>
+
+          <link rel="icon" type="image/png" href="{{ asset('assets/logosdit/sdit_ummatan_wahidah_logo.png') }}">
 
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
