6e2422e88ec012ee76b30903ce2d2a1e5bc9992c
Muhammad Rifky Ramadani <rifkiramadani724@gmail.com>
2026-05-07 15:36:41 +0700

    feat: adding favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
 
     <title>@yield('title', 'Dashboard') - SDITUW</title>
 
+    <link rel="icon" type="image/png" href="{{ asset('assets/logosdit/sdit_ummatan_wahidah_logo.png') }}">
+
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
